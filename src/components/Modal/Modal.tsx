8752eb5865e877307553195bd2330102c25f3beb
Alberto Perez <331258@NTTDATA.COM>
2025-01-20 15:15:51 -0600

    new npm version

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -31,15 +31,15 @@ export const Modal = ({
     return (
         <>
             {isHidden && createPortal(
-                <div id="default-modal" tabIndex={-1} aria-hidden="true" className={classProp + `fixed flex bg-transparent top-0 right-0 left-0 z-50 justify-center items-center w-screen h-full`}>
+                <div id="default-modal" tabIndex={-1} aria-hidden="true" className={classProp + ` fixed flex bg-transparent top-0 right-0 left-0 z-50 justify-center items-center w-screen h-full`}>
                     <div style={{ backgroundColor }} className="p-4 w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <div className="flex flex-row items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                                 {tittle}
                             </h3>
                             <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
 
-                                <svg className="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
 

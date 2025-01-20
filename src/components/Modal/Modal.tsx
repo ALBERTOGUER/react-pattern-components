b6365a82b1a05d35c3d84cfb798748f809f95129
Alberto Perez <331258@NTTDATA.COM>
2025-01-20 15:02:04 -0600

    changes on modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -31,9 +31,22 @@ export const Modal = ({
     return (
         <>
             {isHidden && createPortal(
-                <div id="default-modal" tabIndex={-1} aria-hidden="true" className={classProp + `hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        <div style={{ backgroundColor }} className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div id="default-modal" tabIndex={-1} aria-hidden="true" className={classProp + `fixed flex bg-transparent top-0 right-0 left-0 z-50 justify-center items-center w-screen h-full`}>
+                    <div style={{ backgroundColor }} className="p-4 w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
+                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                {tittle}
+                            </h3>
+                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+
+                                <svg className="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+
+                            </button>
+                        </div>
+                        {children}
+                        {/* <div style={{ backgroundColor }} className="bg-white rounded-lg shadow dark:bg-gray-700">
                             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                                     {tittle}
@@ -47,7 +60,7 @@ export const Modal = ({
                                 </button>
                             </div>
                             {children}
-                        </div>
+                        </div> */}
                     </div>
                 </div>,
                 document.body
